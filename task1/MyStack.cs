22de33a93c6e44827d3f425e06da9d711dd8c4e0
DmitriyTR <skate1233@mail.ru>
2023-11-13 21:24:13 +0300

    Добавьте файлы проекта.

---
+++ task1/MyStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+
+    public class MyStack
+    {
+        private int[] array;  
+        private int top;       
+
+   
+        public MyStack(int size)
+        {
+            array = new int[size];
+            top = -1;  
+        }
+
+       
+        public void Push(int value)
+        {
+            if (top == array.Length - 1)
+            {
+                Console.WriteLine("Стек переполнен. Невозможно добавить элемент.");
+            }
+            else
+            {
+                top++;
+                array[top] = value;
+            }
+        }
+
+        
+        public int Pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Стек пуст. Невозможно извлечь элемент.");
+                return -1;  
+            }
+            else
+            {
+                int poppedValue = array[top];
+                top--;
+                return poppedValue;
+            }
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using task1;
+
+class Program
+{
+    static void Main()
+    {
+        MyStack myStack = new MyStack(5);
+
+        myStack.Push(1);
+        myStack.Push(2);
+        myStack.Push(3);
+
+        Console.WriteLine(myStack.Pop());  
+        Console.WriteLine(myStack.Pop());  
+    }
+}
